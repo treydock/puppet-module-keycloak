fc7e872dec730c62212e75001c8412d1681333c0
Trey Dockendorf <tdockendorf@osc.edu>
2017-09-01 09:08:40 -0400

    Deprecate keycloak::realm defined type which is now just a wrapper for keycloak_realm

--- manifests/realm.pp
+++ manifests/realm.pp
@@ -10,52 +10,20 @@ define keycloak::realm (
   String $email_theme = 'keycloak',
 ) {
 
-  include keycloak
+  warning('Keycloak::Realm is deprecated, use keycloak_realm type directly')
+
+  include ::keycloak
   realize Keycloak_conn_validator['keycloak']
 
-  $config_dir = "${keycloak::install_base}/puppet"
-  $config     = "${config_dir}/realm-${name}.json"
-  $kcadm = "${keycloak::install_base}/bin/kcadm.sh"
-  $auth = "--no-config --server http://localhost:8080/auth --realm master --user admin --password ${keycloak::admin_user_password}"
-
-  # Template uses:
-  # - $name
-  # - $remember_me
-  # - $login_with_email_allowed
-  # - $login_theme
-  # - $account_theme
-  # - $admin_theme
-  # - $email_theme
-  file { $config:
-    ensure  => 'file',
-    owner   => $keycloak::user,
-    group   => $keycloak::group,
-    mode    => '0640',
-    content => template('keycloak/realm.json.erb'),
-    before  => [Exec["create-realm-${name}"], Exec["update-realm-${name}"]],
-  }
-
-  exec { "create-realm-${name}":
-    command   => "${kcadm} create realms -f ${config} ${auth}",
-    unless    => "${kcadm} get realms/${name} ${auth}",
-    cwd       => $keycloak::install_base,
-    user      => $keycloak::user,
-    group     => $keycloak::group,
-    logoutput => true,
-    require   => Keycloak_conn_validator['keycloak'],
-  }
-
-  exec { "update-realm-${name}":
-    command     => "${kcadm} update realms/${name} -f ${config} ${auth}",
-    onlyif      => "${kcadm} get realms/${name} ${auth}",
-    #unless     => "${kcadm} get realms/${name} ${auth} | diff -w ${config} -",
-    cwd         => $keycloak::install_base,
-    user        => $keycloak::user,
-    group       => $keycloak::group,
-    logoutput   => true,
-    require     => Keycloak_conn_validator['keycloak'],
-    refreshonly => true,
-    subscribe   => File[$config],
+  keycloak_realm { $name:
+    display_name             => $display_name,
+    display_name_html        => $display_name_html,
+    remember_me              => $remember_me,
+    login_with_email_allowed => $login_with_email_allowed,
+    login_theme              => $login_theme,
+    account_theme            => $account_theme,
+    admin_theme              => $admin_theme,
+    email_theme              => $email_theme,
   }
 
 }
