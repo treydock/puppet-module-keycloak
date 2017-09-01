11da56e9badbf3079bd8ada0bc5ca651385590e0
Trey Dockendorf <tdockendorf@osc.edu>
2017-09-01 09:03:41 -0400

    Add symlink /opt/keycloak so that future types/providers can have a well defined path to use for kcadm-wrapper.sh Add kcadm-wrapper.sh so that future type/providers don't have to pass passwords over command line and potentially expose in logs or reports

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,6 +2,24 @@
 class keycloak::config {
   assert_private()
 
+  file { '/opt/keycloak':
+    ensure => 'link',
+    target => $keycloak::install_base
+  }
+
+  # Template uses:
+  # - $keycloak::install_base
+  # - $keycloak::admin_user
+  # - $keycloak::admin_user_password
+  file { 'kcadm-wrapper.sh':
+    ensure  => 'file',
+    path    => "${keycloak::install_base}/bin/kcadm-wrapper.sh",
+    owner   => $keycloak::user,
+    group   => $keycloak::group,
+    mode    => '0750',
+    content => template('keycloak/kcadm-wrapper.sh.erb'),
+  }
+
   $_add_user_keycloak_cmd = "${keycloak::install_base}/bin/add-user-keycloak.sh"
   $_add_user_keycloak_args = "--user ${keycloak::admin_user} --password ${keycloak::admin_user_password} --realm master"
   $_add_user_keycloak_state = "${keycloak::install_base}/.create-keycloak-admin-${keycloak::datasource_driver}"
