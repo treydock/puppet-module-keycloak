f90ec565a5d0b962953d38e3569d6163e27d1741
Trey Dockendorf <tdockendorf@osc.edu>
2017-09-01 09:05:51 -0400

    Add keycloak_api type that could be used to share data between future keycloak types/providers

---
+++ lib/puppet/type/keycloak_api.rb
@@ -0,0 +1,47 @@
+Dir[File.dirname(__FILE__) + '/keycloak*.rb'].each do |file|
+  next if file == __FILE__
+  next if File.basename(file) == 'keycloak_conn_validator.rb'
+  require file
+end
+
+Puppet::Type.newtype(:keycloak_api) do
+  newparam(:name, :namevar => true) do
+    desc 'Keycloak API config'
+  end
+
+  newparam(:install_base) do
+  end
+
+  newparam(:server) do
+    defaultto('http://localhost:8080/auth')
+  end
+
+  newparam(:realm) do
+    defaultto('master')
+  end
+
+  newparam(:user) do
+    defaultto('admin')
+  end
+
+  newparam(:password) do
+    defaultto('changeme')
+  end
+
+  def generate
+    [
+      :keycloak_realm,
+      :keycloak_client_template,
+    ].each do |res_type|
+      provider_class = Puppet::Type.type(res_type).provider(:kcadm)
+      provider_class.install_base = self[:install_base]
+      provider_class.server = self[:server]
+      provider_class.realm = self[:realm]
+      provider_class.user = self[:user]
+      provider_class.password = self[:password]
+    end
+
+    []
+  end
+
+end
