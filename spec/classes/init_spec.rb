041c5154a991ca7462494fa0ed4e1cf73f725fae
Trey Dockendorf <tdockendorf@osc.edu>
2017-12-06 16:30:51 -0500

    Set default version to 3.4.1.Final

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -42,8 +42,8 @@ describe 'keycloak' do
       context "keycloak::config" do
         it do
           is_expected.to contain_exec('create-keycloak-admin').with({
-            :command => '/opt/keycloak-3.0.0.Final/bin/add-user-keycloak.sh --user admin --password changeme --realm master && touch /opt/keycloak-3.0.0.Final/.create-keycloak-admin-h2',
-            :creates => '/opt/keycloak-3.0.0.Final/.create-keycloak-admin-h2',
+            :command => '/opt/keycloak-3.4.1.Final/bin/add-user-keycloak.sh --user admin --password changeme --realm master && touch /opt/keycloak-3.4.1.Final/.create-keycloak-admin-h2',
+            :creates => '/opt/keycloak-3.4.1.Final/.create-keycloak-admin-h2',
             :notify  => 'Class[Keycloak::Service]',
           })
         end
