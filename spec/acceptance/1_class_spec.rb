61ed6aa3b80bfca654d78eaf157e61c263cb4650
Trey Dockendorf <tdockendorf@osc.edu>
2017-12-18 10:50:46 -0500

    Fix configuration order when proxy_https is true

--- spec/acceptance/1_class_spec.rb
+++ spec/acceptance/1_class_spec.rb
@@ -54,4 +54,32 @@ describe 'keycloak class:' do
       it { should be_listening.on('127.0.0.1').with('tcp') }
     end
   end
+
+  context 'default with proxy_https' do
+    it 'should run successfully' do
+      pp =<<-EOS
+      include mysql::server
+      class { 'keycloak':
+        datasource_driver => 'mysql',
+        proxy_https       => true,
+      }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe service('keycloak') do
+      it { should be_enabled }
+      it { should be_running }
+    end
+
+    describe port(8080) do
+      it { should be_listening.on('0.0.0.0').with('tcp') }
+    end
+
+    describe port(9990) do
+      it { should be_listening.on('127.0.0.1').with('tcp') }
+    end
+  end
 end
