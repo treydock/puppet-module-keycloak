8bb254fe97d0674539bfa041fe639b98e0205c5a
Trey Dockendorf <tdockendorf@osc.edu>
2018-12-12 15:15:57 -0500

    Fix keycloak_ldap_user_provider bind_credential property to be idempotent

--- spec/acceptance/3_ldap_spec.rb
+++ spec/acceptance/3_ldap_spec.rb
@@ -91,4 +91,82 @@ describe 'keycloak_ldap_user_provider:' do
       end
     end
   end
+
+  context 'creates ldap with simple auth' do
+    it 'should run successfully' do
+      pp =<<-EOS
+      include mysql::server
+      class { 'keycloak':
+        datasource_driver => 'mysql',
+      }
+      keycloak_realm { 'test': ensure => 'present' }
+      keycloak_ldap_user_provider { 'LDAP2':
+        realm => 'test',
+        users_dn => 'ou=People,dc=test',
+        connection_url => 'ldap://localhost:389',
+        custom_user_search_filter => '(objectClass=posixAccount)',
+        auth_type => 'simple',
+        bind_dn => 'cn=read,ou=People,dc=test',
+        bind_credential => 'test',
+      }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    it 'should have created a LDAP user provider' do
+      on hosts, '/opt/keycloak/bin/kcadm-wrapper.sh get components/LDAP2-test -r test' do
+        data = JSON.parse(stdout)
+        expect(data['config']['authType']).to eq(['simple'])
+        expect(data['config']['bindDn']).to eq(['cn=read,ou=People,dc=test'])
+        expect(data['config']['bindCredential'][0]).to match(/^[\*]+$/)
+      end
+    end
+
+    it 'should have set bindCredential' do
+      on hosts, "mysql keycloak -BN -e 'SELECT VALUE FROM COMPONENT_CONFIG WHERE NAME=\"bindCredential\" AND COMPONENT_ID=\"LDAP2-test\"'" do
+        expect(stdout).to match(/^test$/)
+      end
+    end
+  end
+
+  context 'updates ldap auth' do
+    it 'should run successfully' do
+      pp =<<-EOS
+      include mysql::server
+      class { 'keycloak':
+        datasource_driver => 'mysql',
+      }
+      keycloak_realm { 'test': ensure => 'present' }
+      keycloak_ldap_user_provider { 'LDAP':
+        realm => 'test',
+        users_dn => 'ou=People,dc=test',
+        connection_url => 'ldap://localhost:389',
+        user_object_classes => ['posixAccount'],
+        auth_type => 'simple',
+        bind_dn => 'cn=read,ou=People,dc=test',
+        bind_credential => 'test',
+      }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    it 'should have updated a LDAP user provider' do
+      on hosts, '/opt/keycloak/bin/kcadm-wrapper.sh get components/LDAP-test -r test' do
+        data = JSON.parse(stdout)
+        expect(data['config']['authType']).to eq(['simple'])
+        expect(data['config']['bindDn']).to eq(['cn=read,ou=People,dc=test'])
+        expect(data['config']['bindCredential'][0]).to match(/^[\*]+$/)
+      end
+    end
+
+    it 'should have set bindCredential' do
+      on hosts, "mysql keycloak -BN -e 'SELECT VALUE FROM COMPONENT_CONFIG WHERE NAME=\"bindCredential\" AND COMPONENT_ID=\"LDAP-test\"'" do
+        expect(stdout).to match(/^test$/)
+      end
+    end
+  end
 end
