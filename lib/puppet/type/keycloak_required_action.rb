257f945f795956a5e368506d90dfd97e1a08c7de
Evgeny Soynov <saboteur@saboteur.me>
2020-08-06 15:19:57 +0200

    Removed unused require

--- lib/puppet/type/keycloak_required_action.rb
+++ lib/puppet/type/keycloak_required_action.rb
@@ -1,5 +1,4 @@
 require_relative '../../puppet_x/keycloak/type'
-require_relative '../../puppet_x/keycloak/array_property'
 require_relative '../../puppet_x/keycloak/integer_property'
 
 Puppet::Type.newtype(:keycloak_required_action) do
