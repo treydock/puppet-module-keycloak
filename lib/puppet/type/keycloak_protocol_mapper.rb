17c56ac7c0b3958e59a4158f67f02be4e755668d
Benjamin Rechsteiner <38490328+aba-rechsteiner@users.noreply.github.com>
2020-10-27 14:24:13 +0100

    Update lib/puppet/type/keycloak_protocol_mapper.rb
    Co-authored-by: treydock <treydock@gmail.com>

--- lib/puppet/type/keycloak_protocol_mapper.rb
+++ lib/puppet/type/keycloak_protocol_mapper.rb
@@ -245,7 +245,7 @@ Manage Keycloak client scope protocol mappers
       'oidc-full-name-mapper',
       'oidc-group-membership-mapper',
       'oidc-audience-mapper',
-      'oidc-usermodel-attribute-mapper'
+      'oidc-usermodel-attribute-mapper',
     ]
     if self[:protocol] == 'openid-connect' && !openid_connect_types.include?(self[:type])
       raise Puppet::Error, "type #{self[:type]} is not valid for protocol openid-connect"
