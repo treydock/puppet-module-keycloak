39d1e2355f47e7ee0fe4f2a5df79be66b2489f2e
Samuli Seppänen <samuli@openvpn.net>
2021-02-03 16:45:34 +0200

    PoC of multi-machine tests work now
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- spec/acceptance/1_domain_mode_cluster_spec.rb
+++ spec/acceptance/1_domain_mode_cluster_spec.rb
@@ -1,26 +1,40 @@
 require 'spec_helper_acceptance'
 
 describe 'keycloak domain mode cluster', if: RSpec.configuration.keycloak_domain_mode_cluster do
-  node = hosts_as('keycloak-master-el7')
-  context 'default with domain master' do
-    it 'runs successfully' do
-      pp = <<-EOS
-      class { 'keycloak':
-        operating_mode        => 'domain',
-        role                  => 'master',
-        datasource_driver     => 'postgresql',
-        wildfly_user          => 'wildfly',
-        wildfly_user_password => 'wildfly',
+  domain_master = find_only_one('domain_master')
+  domain_slave = find_only_one('domain_slave')
+  p domain_master.class
+  p domain_slave.class
+
+  context 'domain test' do
+    it 'replication is successful' do
+      master_pp = <<-EOS
+      file { '/tmp/master':
+        ensure => 'present',
       }
       EOS
 
-      apply_manifest(pp, catch_failures: true)
-      apply_manifest(pp, catch_changes: true)
-    end
+      #master_pp = <<-EOS
+      #class { 'keycloak':
+      #  operating_mode        => 'domain',
+      #  role                  => 'master',
+      #  datasource_driver     => 'postgresql',
+      #  wildfly_user          => 'wildfly',
+      #  wildfly_user_password => 'wildfly',
+      #}
 
-    describe service('keycloak') do
-      it { is_expected.to be_enabled }
-      it { is_expected.to be_running }
+      slave_pp = <<-EOS
+      file { '/tmp/slave':
+        ensure => 'present',
+      }
+      EOS
+
+      on domain_master, 'echo foo > /tmp/echo-master'
+      on domain_slave, 'echo foo > /tmp/echo-slave'
+
+      apply_manifest_on(domain_master, master_pp, catch_changes: true)
+      apply_manifest_on(domain_master, master_pp, catch_failures: true)
+      apply_manifest_on(domain_slave,  slave_pp,  catch_failures: true)
     end
   end
 end
