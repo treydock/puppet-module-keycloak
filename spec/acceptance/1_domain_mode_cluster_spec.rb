36377681dc3cb09b8c7cf75bc26975e5ee317d6e
Samuli Seppänen <samuli@openvpn.net>
2021-02-04 09:40:58 +0000

    Make domain mode acceptance tests work
    The database instance was missing so of course JDBC_PING could not work
    -> slave never came up.
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- spec/acceptance/1_domain_mode_cluster_spec.rb
+++ spec/acceptance/1_domain_mode_cluster_spec.rb
@@ -3,30 +3,99 @@ require 'spec_helper_acceptance'
 describe 'keycloak domain mode cluster', if: RSpec.configuration.keycloak_domain_mode_cluster do
   domain_master = find_only_one('domain_master')
   domain_slave = find_only_one('domain_slave')
+  db = find_only_one('db')
 
   context 'new cluster' do
     it 'should launch' do
+      db_pp = <<-EOS
+      class { '::postgresql::globals':
+        manage_package_repo => true,
+        version             => '9.6',
+      }
+      
+      class { '::postgresql::server':
+        listen_addresses => '*',
+        require          => Class['::postgresql::globals']
+      }
+      
+      ::postgresql::server::role { 'keycloak':
+        password_hash    => postgresql_password('keycloak', 'keycloak'),
+        connection_limit => 300,
+        require          => Class['::postgresql::server']
+      }
+      
+      ::postgresql::server::database_grant { 'Grant all to keycloak':
+        privilege => 'ALL',
+        db        => 'keycloak',
+        role      => 'keycloak',
+      }
+      
+      ::postgresql::server::db { 'keycloak':
+        user     => 'keycloak',
+        password => postgresql_password('keycloak', 'keycloak'),
+      }
+      
+      postgresql::server::pg_hba_rule { 'Allow Keycloak instances network access to the database':
+        description => 'Open up PostgreSQL for access from anywhere',
+        type        => 'host',
+        database    => 'keycloak',
+        user        => 'keycloak',
+        address     => '0.0.0.0/0',
+        auth_method => 'md5',
+        require     => Class['::postgresql::server']
+      }
+      EOS
+
       master_pp = <<-EOS
-      class { 'keycloak':
-        operating_mode        => 'domain',
-        role                  => 'master',
-        datasource_driver     => 'postgresql',
-        wildfly_user          => 'wildfly',
-        wildfly_user_password => 'wildfly',
+      class { '::keycloak':
+        operating_mode          => 'domain',
+        role                    => 'master',
+        management_bind_address => $::ipaddress,
+        enable_jdbc_ping        => true,
+        wildfly_user            => 'wildfly',
+        wildfly_user_password   => 'wildfly',
+        manage_install          => true,
+        manage_datasource       => false,
+        version                 => '10.0.1',
+        datasource_driver       => 'postgresql',
+        datasource_host         => 'centos-7-db',
+        datasource_port         => 5432,
+        datasource_dbname       => 'keycloak',
+        datasource_username     => 'keycloak',
+        datasource_password     => 'keycloak',
+        admin_user              => 'admin',
+        admin_user_password     => 'changeme',
+        service_bind_address    => '0.0.0.0',
+        proxy_https             => false,
       }
       EOS
 
       slave_pp = <<-EOS
-      class { 'keycloak':
-        operating_mode        => 'domain',
-        role                  => 'slave',
-	    master_address        => 'centos-7-master',
-        datasource_driver     => 'postgresql',
-        wildfly_user          => 'wildfly',
-        wildfly_user_password => 'wildfly',
+      class { '::keycloak':
+        operating_mode          => 'domain',
+        role                    => 'slave',
+        enable_jdbc_ping        => true,
+        management_bind_address => $::ipaddress,
+        wildfly_user            => 'wildfly',
+        wildfly_user_password   => 'wildfly',
+        master_address          => 'centos-7-master',
+        manage_install          => true,
+        manage_datasource       => false,
+        version                 => '10.0.1',
+        datasource_driver       => 'postgresql',
+        datasource_host         => 'centos-7-db',
+        datasource_port         => 5432,
+        datasource_dbname       => 'keycloak',
+        datasource_username     => 'keycloak',
+        datasource_password     => 'keycloak',
+        admin_user              => 'admin',
+        admin_user_password     => 'changeme',
+        service_bind_address    => '0.0.0.0',
+        proxy_https             => false,
       }
       EOS
 
+      apply_manifest_on(db, db_pp, catch_failures: true)
       apply_manifest_on(domain_master, master_pp, catch_failures: true)
       apply_manifest_on(domain_master, master_pp, catch_changes: true)
       apply_manifest_on(domain_slave,  slave_pp,  catch_failures: true)
@@ -41,6 +110,5 @@ describe 'keycloak domain mode cluster', if: RSpec.configuration.keycloak_domain
       it { is_expected.to be_enabled }
       it { is_expected.to be_running }
     end
-
   end
 end
