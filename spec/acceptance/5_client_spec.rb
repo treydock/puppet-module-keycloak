fc2becc0fecf2554b33117bd734be765a6a37e6c
Robert Wolf <robert.wolf@abacus.ch>
2022-03-31 16:06:40 +0200

    last try indentation

--- spec/acceptance/5_client_spec.rb
+++ spec/acceptance/5_client_spec.rb
@@ -125,69 +125,46 @@ describe 'keycloak_client define:', if: RSpec.configuration.keycloak_full do
         expect(data['attributes']['saml_assertion_consumer_url_post']).to eq('https://saml.foo.bar/mellon/postResponse')
         expect(data['attributes']['saml.encrypt']).to eq('true')
         expect(data['attributes']['saml.assertion.signature']).to eq('true')
-        expect(data['attributes']['saml.signing.certificate']).to \
-          eq('MIIDQzCCAiugAwIBAgIUNALBnAmwcPKLdBer4e0i22JiEd0wDQYJKoZIhvcNAQE' \
-          'LBQAwMDEuMCwGA1UEAwwlc2FtbF9zaWduaW5nX2NlcnRpZmljYXRlLXRlc3QuZm9vL' \
-          'mJhcjAgFw0yMjAzMTgyMjU1MTNaGA8yMTIyMDIyMjIyNTUxM1owMDEuMCwGA1UEAww' \
-          'lc2FtbF9zaWduaW5nX2NlcnRpZmljYXRlLXRlc3QuZm9vLmJhcjCCASIwDQYJKoZIh' \
-          'vcNAQEBBQADggEPADCCAQoCggEBAPKEr/vAExQ9LxF9oiiyz7JNdHgB8wxGEd5SN7Y' \
-          'F6UOToLH2lQL5PelGnsVD13y9/ZN2qr3xl54zz8IT8EimT8YXc0k7JEDLnKrvkz/xG' \
-          'SKnSFnudNM7B6i2VgM7uMbCCKKbOlULALUUnUmOAsKjRyTjcue4D1tULnha+ph+h/1' \
-          'T9Oc0VmDf6BV54lEwOe7m7teOZCTnrM2Ll2dsZV2tgMywt87r9/yQWt3rbMjaVsLUs' \
-          'gV/SBd4RU6WDrsae6P7ccOWeoFyQ4fP7i7Z+Wpa8Y9pdnNraqBNmkKb4pNkW+sJkkf' \
-          'A47YHafAtQO4E1cOnlsKJo0fXeMgiViCoDLVYPwcCAwEAAaNTMFEwHQYDVR0OBBYEF' \
-          'MuSde+sXzqeWtZPfY7Bsun3h3fzMB8GA1UdIwQYMBaAFMuSde+sXzqeWtZPfY7Bsun' \
-          '3h3fzMA8GA1UdEwEB/wQFMAMBAf8wDQYJKoZIhvcNAQELBQADggEBAF+KdjMVaLGJm' \
-          'bBpV+mw6G9QVZ8DkiMKKz5+K2usCMCVf9XWVIyYXExsTBACk1FObHjXfHkk1A9nXOE' \
-          'FqgPBVS8CgSspVfQjIGhDy1lwhPkDvtyuIKIDb2kf52xTru/bsAWuSEXcjbKqszN6l' \
-          '78AaR93n6sZKUkIsnDpvi/mG4xXBumiluJyedbQw9yj/NsYouALGWWQeHDwNJGcAxD' \
-          'xiLvIZjXkAo6IXRQ85n29TiLFCbF5hPmiBlUNfo5reuobvhd+qDZOssiJ4q4VrSkHZ' \
-          /u4Ri623+dgyZbDqwQ20NigfuoRaxbFWxbjl5T/lg30r7sqy/YOW2wevA0wSqyxMfw=')
-        expect(data['attributes']['saml.encryption.certificate']).to \
-          eq('MIIDSTCCAjGgAwIBAgIUbJ6dLiM4/T9uLT4gd13tuD469lkwDQYJKoZIhvcNAQE' \
-          'LBQAwMzExMC8GA1UEAwwoc2FtbF9lbmNyeXB0aW9uX2NlcnRpZmljYXRlLXRlc3QuZ' \
-          'm9vLmJhcjAgFw0yMjAzMTgyMjU1NDlaGA8yMTIyMDIyMjIyNTU0OVowMzExMC8GA1U' \
-          'EAwwoc2FtbF9lbmNyeXB0aW9uX2NlcnRpZmljYXRlLXRlc3QuZm9vLmJhcjCCASIwD' \
-          'QYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKmzbda8/vwS3vn1OflWhcquzhh+Fph' \
-          'TCA4PoRsqT2/AE5cbEPqVdPQxIUVXNL+l4LM7Kl4H0vSoi3gHlH1SQCc/772gXDtBy' \
-          'xHP0QYg+FaEIG+LbsFYeB1jJMhGblf+0xOH3uPtN9jzjkz8Xhzpcq/xgTOJDyQPfSa' \
-          'mzW0xUtK3iXd8B7K9nNdmOm9uLPZ1p2JLhvOJu6I6dapjLDoWgJnSnaYMgRuxShktT' \
-          'afWU3wolyo6c6+wago/CaoEdlrcwO7VvOd/gdhAuYUhYypD7t+1mWisEBWxLo2omfl' \
-          'r2rm2nWQX5EKx4U1lhEPxxlo0AkCCj/7hQyJt5jMzg/4QGNUCAwEAAaNTMFEwHQYDV' \
-          'R0OBBYEFMNcZ9lzmttxhrdVXLm+deYLJyjeMB8GA1UdIwQYMBaAFMNcZ9lzmttxhrd' \
-          'VXLm+deYLJyjeMA8GA1UdEwEB/wQFMAMBAf8wDQYJKoZIhvcNAQELBQADggEBAEeYM' \
-          'FSGUGeWkgNKTmPL3l445ai4zMWUi92+vHeta44GlBKUAbNvC8Ab4mdVFCZF0lvKqUV' \
-          'feKtTDD9wSko5XjjuhblLci7oa/gOFpp3dfb5d5xtNsSoVD9ndPopApYugGlKEJI9q' \
-          'L39FyP9Js+rm13gsHNrMGXIfBE+FSFXu0sp0NRBnbqvz5cB8jSRb40v67tGmFVadYh' \
-          'omIpFsES2FuM3bY6YbD0hJ4ozLczgpfPOYw41xIAGSgbB6rwRsu+VwV7L2DW3wtq6C' \
-          'HksLYoiNDmdZXz0HDqmfHcMKlpUPpAkY/8q6xaO/QNEzohI60TfMRklpCLP/25n4ao' \
-          '3VqrHdZo=')
-        expect(data['attributes']['saml.signing.private.key']).to \
-          eq('MIIEpAIBAAKCAQEAtzXe4xaXsz5KdSQdz/4+jMdO1HiBNBuL2dIQ4b+DSP5IhSU' \
-          '+VPQP26G49fBWkN2ZSGMhOfvfxbcGjudIl7RRKRN1XqTgada/irqhU80Z8FTYWgl6A' \
-          '5px87nL1peCm8f2w6N220KLdoYI/KapdNP1CUXR6iBJOrEZ3lV3CtZR5DkeOvdMEsm' \
-          'yhP5ajI4PMKU15ANmq8S7vPd2q/OGSQziAj467gDFDTXTWVVo1vV1HWSz9an2wdIU7' \
-          'XdgrzRbuuCvgb5LLpbdyy+3j3RieoQAiDAiabKZqSMhiYc6mx21tD7ppN+H6RqzRul' \
-          'j/7WLdxckEJ736s1xSk8boOQhEqPoIQIDAQABAoIBAQCn3aQrTjgQ87IlQsJOIRYOx' \
-          '09jPkakB9lL6z2sml0gNF0eIdHK5RTimHtwXJX0hhY8TRfUmQOflONdbG0HEyCKElo' \
-          'oLcmxMCKwafAHaJWrrxHM7YHua0SdnE84f/ob4kwnVU9B9ubx4j25wLrjYJHTvTVo3' \
-          '8w5Cqw5GvXH6DeAc4gg0xtl1kYy53xO+3ybZDZy7EpEKFbgSLNZIryrIk3v1v2uWQW' \
-          'j30Gb2OqppTRt2X+zSSph5MUqPIfKUweL3Ow2MbCBdNESp8QifdUld1RWEgVbZWSvc' \
-          'ZmfYpr9w/rU+JVrVydmCBx0k8VWJmBobZydr4niVLhkhuzY8DvfLyJBAoGBAOFijou' \
-          '/Mh2B4mRdycwOxBcJUeL318K5MR6ondidP5SEcHpTavaQjqE9N+I0W0GPsJhzuBiUH' \
-          'q60TjC5GV3OYTp/G+4M4ibVYyR9UG3CrHAlgu9asakVfcOHhOJmyyD1FtI4BlNNI+n' \
-          'v4Ds7nHahv3yFSF/pes9VaAKm9k5ZTzmXAoGBANAYwOW8obHCLmHcrJyHcZX9icJAX' \
-          'P+GvmpsJpZaBSE2bs/K2cwLZnBoiUX1jH+7wWtUV2CfATMAD2UASocaYrSSF91bZAJ' \
-          'Ne3D9QliRC10IZ6SBn1Pw7u3uYVHb7JE6z4IGIrn503En5ncZcxfoc0JE9bNIM3tyE' \
-          'ygetS/EifMHAoGAAp0Z+hTlh+IRtghAZtVlAL9i67bkEaYEI87gxbpNGnPOuhxtiR5' \
-          '0CPqkw0LILCJ2cc4lvGM7V9tPbNE4shXKmtsOf9w2YyzmUW4CmMNBLKvCsPPkS4msQ' \
-          '7A2okl+4Yr2EMoFiMHEQNo/R3CRh+6oQdFp3XLfsbfT1PQKty3h9VECgYEAn6euL1R' \
-          '21fO+NDTjdcBww/veelt5Pk65vtq1DDuKnf2uLNxcFzFT6cA6OaN3pPR/JAJ0e1vix' \
-          'qcwKHR9uYPj4NgJWTpp015w67JS+bJmfn0ZT1xnyjYaig+POQe7S31MgVyFvhvPPoy' \
-          '3Q/8Rj3E3JMvVmjQ102slCW3t4vUuRXcCgYAkfbK84PXNvDWXQFR27fWJsLwLzORYx' \
-          'nk0l4oFOOJy926m/WMOfw33pVhsJInHK+iRKwPv33zo5YkB1BeGWedvM/gAgq5eSo+' \
-          'eqSkKn5M+eaDTlvWFrDK5tW21m49wtYOKDGo99tjgfaoJDDhGkX0NdKZ23BEvW1AIn' \
-          'hCPNyE5rg==')
+        expect(data['attributes']['saml.signing.certificate']).to eq('MIIDQzCCAiugAwIBAgIUNALBnAmwcPKLdBer4e0i22JiEd0' \
+          'wDQYJKoZIhvcNAQELBQAwMDEuMCwGA1UEAwwlc2FtbF9zaWduaW5nX2NlcnRpZmljYXRlLXRlc3QuZm9vLmJhcjAgFw0yMjAzMTgyMjU1M' \
+          'TNaGA8yMTIyMDIyMjIyNTUxM1owMDEuMCwGA1UEAwwlc2FtbF9zaWduaW5nX2NlcnRpZmljYXRlLXRlc3QuZm9vLmJhcjCCASIwDQYJKoZ' \
+          'IhvcNAQEBBQADggEPADCCAQoCggEBAPKEr/vAExQ9LxF9oiiyz7JNdHgB8wxGEd5SN7YF6UOToLH2lQL5PelGnsVD13y9/ZN2qr3xl54zz' \
+          '8IT8EimT8YXc0k7JEDLnKrvkz/xGSKnSFnudNM7B6i2VgM7uMbCCKKbOlULALUUnUmOAsKjRyTjcue4D1tULnha+ph+h/1T9Oc0VmDf6BV' \
+          '54lEwOe7m7teOZCTnrM2Ll2dsZV2tgMywt87r9/yQWt3rbMjaVsLUsgV/SBd4RU6WDrsae6P7ccOWeoFyQ4fP7i7Z+Wpa8Y9pdnNraqBNm' \
+          'kKb4pNkW+sJkkfA47YHafAtQO4E1cOnlsKJo0fXeMgiViCoDLVYPwcCAwEAAaNTMFEwHQYDVR0OBBYEFMuSde+sXzqeWtZPfY7Bsun3h3f' \
+          'zMB8GA1UdIwQYMBaAFMuSde+sXzqeWtZPfY7Bsun3h3fzMA8GA1UdEwEB/wQFMAMBAf8wDQYJKoZIhvcNAQELBQADggEBAF+KdjMVaLGJm' \
+          'bBpV+mw6G9QVZ8DkiMKKz5+K2usCMCVf9XWVIyYXExsTBACk1FObHjXfHkk1A9nXOEFqgPBVS8CgSspVfQjIGhDy1lwhPkDvtyuIKIDb2k' \
+          'f52xTru/bsAWuSEXcjbKqszN6l78AaR93n6sZKUkIsnDpvi/mG4xXBumiluJyedbQw9yj/NsYouALGWWQeHDwNJGcAxDxiLvIZjXkAo6IX' \
+          'RQ85n29TiLFCbF5hPmiBlUNfo5reuobvhd+qDZOssiJ4q4VrSkHZ/u4Ri623+dgyZbDqwQ20NigfuoRaxbFWxbjl5T/lg30r7sqy/YOW2w' \
+          'evA0wSqyxMfw=')
+        expect(data['attributes']['saml.encryption.certificate']).to eq('MIIDSTCCAjGgAwIBAgIUbJ6dLiM4/T9uLT4gd13tuD46' \
+          '9lkwDQYJKoZIhvcNAQELBQAwMzExMC8GA1UEAwwoc2FtbF9lbmNyeXB0aW9uX2NlcnRpZmljYXRlLXRlc3QuZm9vLmJhcjAgFw0yMjAzMT' \
+          'gyMjU1NDlaGA8yMTIyMDIyMjIyNTU0OVowMzExMC8GA1UEAwwoc2FtbF9lbmNyeXB0aW9uX2NlcnRpZmljYXRlLXRlc3QuZm9vLmJhcjCC' \
+          'ASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKmzbda8/vwS3vn1OflWhcquzhh+FphTCA4PoRsqT2/AE5cbEPqVdPQxIUVXNL+l4L' \
+          'M7Kl4H0vSoi3gHlH1SQCc/772gXDtByxHP0QYg+FaEIG+LbsFYeB1jJMhGblf+0xOH3uPtN9jzjkz8Xhzpcq/xgTOJDyQPfSamzW0xUtK3' \
+          'iXd8B7K9nNdmOm9uLPZ1p2JLhvOJu6I6dapjLDoWgJnSnaYMgRuxShktTafWU3wolyo6c6+wago/CaoEdlrcwO7VvOd/gdhAuYUhYypD7t' \
+          '+1mWisEBWxLo2omflr2rm2nWQX5EKx4U1lhEPxxlo0AkCCj/7hQyJt5jMzg/4QGNUCAwEAAaNTMFEwHQYDVR0OBBYEFMNcZ9lzmttxhrdV' \
+          'XLm+deYLJyjeMB8GA1UdIwQYMBaAFMNcZ9lzmttxhrdVXLm+deYLJyjeMA8GA1UdEwEB/wQFMAMBAf8wDQYJKoZIhvcNAQELBQADggEBAE' \
+          'eYMFSGUGeWkgNKTmPL3l445ai4zMWUi92+vHeta44GlBKUAbNvC8Ab4mdVFCZF0lvKqUVfeKtTDD9wSko5XjjuhblLci7oa/gOFpp3dfb5' \
+          'd5xtNsSoVD9ndPopApYugGlKEJI9qL39FyP9Js+rm13gsHNrMGXIfBE+FSFXu0sp0NRBnbqvz5cB8jSRb40v67tGmFVadYhomIpFsES2Fu' \
+          'M3bY6YbD0hJ4ozLczgpfPOYw41xIAGSgbB6rwRsu+VwV7L2DW3wtq6CHksLYoiNDmdZXz0HDqmfHcMKlpUPpAkY/8q6xaO/QNEzohI60Tf' \
+          'MRklpCLP/25n4ao3VqrHdZo=')
+        expect(data['attributes']['saml.signing.private.key']).to eq('MIIEpAIBAAKCAQEAtzXe4xaXsz5KdSQdz/4+jMdO1HiBNBu' \
+          'L2dIQ4b+DSP5IhSU+VPQP26G49fBWkN2ZSGMhOfvfxbcGjudIl7RRKRN1XqTgada/irqhU80Z8FTYWgl6A5px87nL1peCm8f2w6N220KLd' \
+          'oYI/KapdNP1CUXR6iBJOrEZ3lV3CtZR5DkeOvdMEsmyhP5ajI4PMKU15ANmq8S7vPd2q/OGSQziAj467gDFDTXTWVVo1vV1HWSz9an2wdI' \
+          'U7XdgrzRbuuCvgb5LLpbdyy+3j3RieoQAiDAiabKZqSMhiYc6mx21tD7ppN+H6RqzRulj/7WLdxckEJ736s1xSk8boOQhEqPoIQIDAQABA' \
+          'oIBAQCn3aQrTjgQ87IlQsJOIRYOx09jPkakB9lL6z2sml0gNF0eIdHK5RTimHtwXJX0hhY8TRfUmQOflONdbG0HEyCKElooLcmxMCKwafA' \
+          'HaJWrrxHM7YHua0SdnE84f/ob4kwnVU9B9ubx4j25wLrjYJHTvTVo38w5Cqw5GvXH6DeAc4gg0xtl1kYy53xO+3ybZDZy7EpEKFbgSLNZI' \
+          'ryrIk3v1v2uWQWj30Gb2OqppTRt2X+zSSph5MUqPIfKUweL3Ow2MbCBdNESp8QifdUld1RWEgVbZWSvcZmfYpr9w/rU+JVrVydmCBx0k8V' \
+          'WJmBobZydr4niVLhkhuzY8DvfLyJBAoGBAOFijou/Mh2B4mRdycwOxBcJUeL318K5MR6ondidP5SEcHpTavaQjqE9N+I0W0GPsJhzuBiUH' \
+          'q60TjC5GV3OYTp/G+4M4ibVYyR9UG3CrHAlgu9asakVfcOHhOJmyyD1FtI4BlNNI+nv4Ds7nHahv3yFSF/pes9VaAKm9k5ZTzmXAoGBANA' \
+          'YwOW8obHCLmHcrJyHcZX9icJAXP+GvmpsJpZaBSE2bs/K2cwLZnBoiUX1jH+7wWtUV2CfATMAD2UASocaYrSSF91bZAJNe3D9QliRC10IZ' \
+          '6SBn1Pw7u3uYVHb7JE6z4IGIrn503En5ncZcxfoc0JE9bNIM3tyEygetS/EifMHAoGAAp0Z+hTlh+IRtghAZtVlAL9i67bkEaYEI87gxbp' \
+          'NGnPOuhxtiR50CPqkw0LILCJ2cc4lvGM7V9tPbNE4shXKmtsOf9w2YyzmUW4CmMNBLKvCsPPkS4msQ7A2okl+4Yr2EMoFiMHEQNo/R3CRh' \
+          '+6oQdFp3XLfsbfT1PQKty3h9VECgYEAn6euL1R21fO+NDTjdcBww/veelt5Pk65vtq1DDuKnf2uLNxcFzFT6cA6OaN3pPR/JAJ0e1vixqc' \
+          'wKHR9uYPj4NgJWTpp015w67JS+bJmfn0ZT1xnyjYaig+POQe7S31MgVyFvhvPPoy3Q/8Rj3E3JMvVmjQ102slCW3t4vUuRXcCgYAkfbK84' \
+          'PXNvDWXQFR27fWJsLwLzORYxnk0l4oFOOJy926m/WMOfw33pVhsJInHK+iRKwPv33zo5YkB1BeGWedvM/gAgq5eSo+eqSkKn5M+eaDTlvW' \
+          'FrDK5tW21m49wtYOKDGo99tjgfaoJDDhGkX0NdKZ23BEvW1AInhCPNyE5rg==')
       end
     end
   end
