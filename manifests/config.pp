a2e9f6e08ba4577db7adb62641f1de21274bf340
Raphaël Bordet <raphael.bordet.ext@francetv.fr>
2022-05-09 16:41:05 +0200

    Only define parents directories for domain mode

--- manifests/config.pp
+++ manifests/config.pp
@@ -48,13 +48,12 @@ class keycloak::config {
       dirname($_server_conf_dir)
     ]
 
-  }
-
-  file { $_dirs:
-    ensure => 'directory',
-    owner  => $keycloak::user,
-    group  => $keycloak::group,
-    mode   => '0755',
+    file { $_dirs:
+      ensure => 'directory',
+      owner  => $keycloak::user,
+      group  => $keycloak::group,
+      mode   => '0755',
+    }
   }
 
   exec { 'create-keycloak-admin':
