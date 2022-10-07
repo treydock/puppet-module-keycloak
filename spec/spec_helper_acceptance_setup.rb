852628465879c0d4edbf3fd6b4dc793fe965637f
treydock <tdockendorf@osc.edu>
2022-10-07 15:29:01 -0400

    Support MySQL module 13.x (#256)
    * Support MySQL module 13.x
    
    * Allow Ubuntu to use MariaDB

--- spec/spec_helper_acceptance_setup.rb
+++ spec/spec_helper_acceptance_setup.rb
@@ -31,11 +31,6 @@ EOS
 ubuntu1804_yaml = <<-EOS
 keycloak::db: mysql
 EOS
-# TODO: Use until this released to force mariadb:
-# https://github.com/puppetlabs/puppetlabs-mysql/commit/8c8c01739f593b2bcd1943297761a09dde994197
-ubuntu2004_yaml = <<-EOS
-keycloak::db: mysql
-EOS
 common_yaml = <<-EOS
 ---
 keycloak::version: '#{RSpec.configuration.keycloak_version}'
@@ -52,4 +47,3 @@ on hosts, 'mkdir -p /etc/puppetlabs/puppet/data/os/CentOS'
 create_remote_file(hosts, '/etc/puppetlabs/puppet/data/os/CentOS/7.yaml', centos7_yaml)
 on hosts, 'mkdir -p /etc/puppetlabs/puppet/data/os/Ubuntu'
 create_remote_file(hosts, '/etc/puppetlabs/puppet/data/os/Ubuntu/18.04.yaml', ubuntu1804_yaml)
-create_remote_file(hosts, '/etc/puppetlabs/puppet/data/os/Ubuntu/20.04.yaml', ubuntu2004_yaml)
