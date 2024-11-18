f29130054a41d6d8f87bf962c9e6a7abfde9fb6b
Trey Dockendorf <tdockendorf@osc.edu>
2024-11-18 08:19:14 -0500

    Fix conditional

--- files/kcadm-wrapper.sh
+++ files/kcadm-wrapper.sh
@@ -6,7 +6,7 @@
 EXPIRES=$(/usr/bin/sed  -n -r 's|.*"refreshExpiresAt" : ([0-9]*).*|\1|p' "$CONFIG" || echo "0")
 NOW=$(/usr/bin/date +%s%3N)
 
-if [ ! -f "$CONFIG" ] || [ "$EXPIRES" -gt "$NOW" ]; then
+if [ ! -f "$CONFIG" ] || [ "$EXPIRES" -lt "$NOW" ]; then
     ${KCADM} config credentials --config "$CONFIG" --server "$SERVER" --realm "$REALM" --user "$ADMIN_USER" --password "$PASSWORD"
 fi
 
