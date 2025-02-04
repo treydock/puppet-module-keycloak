89b61a4fc18866c1b3787d9ced86b7d0cc7adbbd
Robert Wolf <robert.wolf@abacus.ch>
2025-02-04 14:47:24 +0100

    spec files

--- spec/unit/puppet/type/keycloak_client_protocol_mapper_spec.rb
+++ spec/unit/puppet/type/keycloak_client_protocol_mapper_spec.rb
@@ -370,6 +370,27 @@ describe Puppet::Type.type(:keycloak_client_protocol_mapper) do
     }.to raise_error(%r{foo})
   end
 
+  it 'accepts value for aggregate_attrs' do
+    config[:aggregate.attrs] = false
+    expect(resource[:aggregate_attrs]).to eq(:false)
+  end
+
+  it 'accepts value for aggregate_attrs string' do
+    config[:aggregate.attrs] = 'false'
+    expect(resource[:aggregate_attrs]).to eq(:false)
+  end
+
+  it 'has default for aggregate_attrs' do
+    expect(resource[:aggregate_attrs]).to be_nil
+  end
+
+  it 'does not accept invalid value for aggregate_attrs' do
+    config[:aggregate.attrs] = 'foo'
+    expect {
+      resource
+    }.to raise_error(%r{foo})
+  end
+
   it 'accepts script' do
     config[:protocol] = 'saml'
     config[:type] = 'script-foo.js'
