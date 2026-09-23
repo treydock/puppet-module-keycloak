2b3ade819271ba6f09f3be665d9ca93e65fd0a9f
Trey Dockendorf <tdockendorf@osc.edu>
2026-09-23 14:55:32 -0400

    Attempt to fix acceptance - part2

--- spec/spec_helper_acceptance_setup.rb
+++ spec/spec_helper_acceptance_setup.rb
@@ -79,8 +79,21 @@ EL10_YAML
 deb13 = <<-DEB13_YAML
 ---
 mysql::server::override_options:
-  ssl-disable: true
+  client:
+    ssl: false
+  mysqld:
+    ssl-disable: true
 DEB13_YAML
+
+ubuntu26 = <<-UBUNTU26_YAML
+---
+mysql::server::override_options:
+  client:
+    ssl: false
+  mysqld:
+    ssl-disable: true
+postgresql::globals::version: '18'
+UBUNTU26_YAML
 
 create_remote_file(hosts, File.join(puppet_dir, 'hiera.yaml'), hiera_yaml)
 on hosts, "mkdir -p #{File.join(puppet_dir, 'data')}"
@@ -91,6 +104,7 @@ create_remote_file(hosts, File.join(puppet_dir, 'data/os/RedHat/9.yaml'), el9_ya
 create_remote_file(hosts, File.join(puppet_dir, 'data/os/RedHat/10.yaml'), el10_yaml)
 on hosts, "mkdir -p #{File.join(puppet_dir, 'data/os/Debian')}"
 create_remote_file(hosts, File.join(puppet_dir, 'data/os/Debian/13.yaml'), deb13)
+create_remote_file(hosts, File.join(puppet_dir, 'data/os/Debian/26.04.yaml'), ubuntu26)
 
 # Ensure new enough MariaDB is used
 if fact('os.family') == 'RedHat' && fact('os.release.major').to_i < 10
