547242bf63cd6c095b7fb29761db2d5ca1c5661a
Trey Dockendorf <tdockendorf@osc.edu>
2026-09-23 13:04:35 -0400

    Support java 12 dependency and work around lack of Ubuntu 26.04 support

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,6 +25,7 @@
 #   How to declare the Java class within this module
 #   The `include` value only includes the java class
 #   The `class` method defines the Java class and passes necessary parameters
+#   The `none` value will exclude Java class
 #   For RedHat base systems this defaults to `class`, other OSes default to `include`
 # @param java_package
 #   Java package name, only used when `java_declare_method` is `class`
@@ -412,14 +413,22 @@ class keycloak (
     }
   }
 
-  if $java_declare_method == 'include' {
+  if $java_declare_method == 'none' {
+    package { $java_package:
+      ensure => 'installed',
+      before => Class['keycloak::install']
+    }
+  } elsif $java_declare_method == 'include' {
     contain java
+    Class['java']
+    -> Class['keycloak::install']
   } else {
     class { 'java':
       package               => $java_package,
       java_home             => $java_home,
       java_alternative_path => $java_alternative_path,
       java_alternative      => $java_alternative,
+      before                => Class['keycloak::install'],
     }
   }
 
@@ -427,8 +436,7 @@ class keycloak (
   contain 'keycloak::config'
   contain 'keycloak::service'
 
-  Class['java']
-  -> Class['keycloak::install']
+  Class['keycloak::install']
   -> Class['keycloak::config']
   -> Class['keycloak::service']
 
